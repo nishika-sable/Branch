cf54aec3f5cdbc701363000516675aa38807fd96
nishika-sable <nishikasable005@gmail.com>
2026-08-24 11:21:48 +0000

    idk

--- hello.sh
+++ hello.sh
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 hi my name is nishika dadaram sable
 
 
 
 hi this is the 3rd commit in the main branch 
+=======
+hi my name is nishika dadaram sable 
+
+
+Hi this for my second branch app 
+>>>>>>> app
